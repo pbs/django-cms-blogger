f9bf8f6900f562815b87b84bbad4fa4c72ee04db
Laura Feier <feierlaura10@gmail.com>
2015-03-02 14:31:11 +0200

    * added update date which will be used for entries ordering * update date has bigger priority than publication date

--- cms_blogger/templatetags/blogger.py
+++ cms_blogger/templatetags/blogger.py
@@ -21,23 +21,46 @@ def jsonify(obj_to_jsonify):
 jsonify.is_safe = True
 
 
-@register.inclusion_tag('cms_blogger/entry_pub_date.html')
-def publish_date_box(entry):
-    if not hasattr(entry, 'publication_date'):
-        return {}
-    datetime_obj = entry.publication_date
+def as_utc(datetime_obj):
     is_aware = (datetime_obj.tzinfo is not None and
                 datetime_obj.tzinfo.utcoffset(datetime_obj) is not None)
     if is_aware:
         as_utc = datetime_obj.astimezone(tz.tzutc())
     else:
         as_utc = datetime_obj
-    as_utc = as_utc.replace(tzinfo=None)
+    return as_utc.replace(tzinfo=None)
+
+
+def as_millis(datetime_obj):
+    return calendar.timegm(datetime_obj.timetuple()) * 1000
+
+
+@register.inclusion_tag('cms_blogger/entry_pub_date.html')
+def publish_date_box(entry):
+    mod_time = as_utc(entry.update_date)
+    pub_time = as_utc(entry.publication_date)
+    is_updated = mod_time > pub_time
+
+    display_date = mod_time if is_updated else pub_time
     # entry is published for more than 3 months
-    is_older = as_utc < (datetime.datetime.utcnow() +
-                         relativedelta.relativedelta(months=-3))
+    is_older = display_date < (datetime.datetime.utcnow() +
+                               relativedelta.relativedelta(months=-3))
+    var_name = ("entry_pub_%s%s" % (time.time(), entry.id)).replace('.' , '')
     return {
-        'date_var': (
-            "entry_pub_%s%s" % (time.time(), entry.id)).replace('.' , ''),
-        'utc_millis': calendar.timegm(as_utc.timetuple()) * 1000,
-        'show_year': is_older}
+        'date_var': var_name,
+        'utc_millis': as_millis(display_date),
+        'show_year': is_older
+    }
+
+
+@register.inclusion_tag('cms_blogger/entry_author.html')
+def author_row(entry):
+    mod_time = as_utc(entry.update_date)
+    pub_time = as_utc(entry.publication_date)
+    is_updated = mod_time > pub_time
+
+    return {
+        'is_updated': is_updated,
+        'utc_millis': as_millis(mod_time if is_updated else pub_time),
+        'author_names': entry.authors_display_name,
+    }
