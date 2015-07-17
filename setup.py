d7976d95f3eddde2a6a926ec8644ebee93f1b37f
Laura Feier <laura.feier@3pillarglobal.com>
2015-07-17 11:48:37 +0300

    Version Bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DEPENDENCY_LINKS = [
 
 setup(
     name='django-cms-blogger',
-    version='0.7.0.pbs.4',
+    version='0.7.0.pbs.5',
     description='Django CMS blogging tool that lets users create blogs '
                 'using layouts created from CMS pages.',
     long_description=open(README, 'r').read(),
